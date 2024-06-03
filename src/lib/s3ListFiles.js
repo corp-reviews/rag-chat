f1c33ebaa795783b50c461087a643b2a05481736
Kim JungHyun <lallacri86@gmail.com>
2024-06-03 08:36:51 +0000

    Make the title of pdf files downloadable links

--- src/lib/s3ListFiles.js
+++ src/lib/s3ListFiles.js
@@ -1,6 +1,7 @@
 // src/lib/s3ListFiles.js
 import { get } from 'svelte/store';
-import { S3Client, ListObjectsV2Command } from '@aws-sdk/client-s3';
+import { S3Client, ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { accessKeyId, secretAccessKey, region, bucketName } from '../stores/env';
 
 export async function listFilesFromS3() {
@@ -19,7 +20,19 @@ export async function listFilesFromS3() {
     try {
         const command = new ListObjectsV2Command(params);
         const data = await s3Client.send(command);
-        return data.Contents.filter(file => file.Key.endsWith('.pdf')).map(file => file.Key);
+        const files = data.Contents.filter(file => file.Key.endsWith('.pdf')).map(async file => {
+            const getObjectCommand = new GetObjectCommand({
+                Bucket: get(bucketName),
+                Key: file.Key
+            });
+            const url = await getSignedUrl(s3Client, getObjectCommand, { expiresIn: 3600 }); // URL valid for 1 hour
+            return {
+                name: file.Key,
+                url
+            };
+        });
+
+        return await Promise.all(files);
     } catch (error) {
         console.error('Error listing files:', error);
         return [];
