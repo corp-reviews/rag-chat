50309c8db500e90caf4975c1e18d37eb651fae5c
Kim JungHyun <lallacri86@gmail.com>
2024-06-03 12:55:45 +0000

    Change the description of the service

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,9 @@
 <!-- src/components/Header.svelte -->
 
 <header class="flex flex-col items-center justify-center bg-white pt-8 pb-8">
-    <h1 class="flex-1 text-center text-xl">기업정보 챗봇</h1>
+    <h1 class="flex-1 text-center text-xl">탐색 증강 생성형 챗봇</h1>
     <p class="text-gray-500 mt-2 text-center text-sm px-8">
         OpenAI API 키를 준비해 입력한 다음, 응답을 받는 데에 사용할 GPT 모델을 선택하세요.
-        회사를 선택하고 질문을 남기시면, DB 정보를 바탕으로 답변을 확인할 수 있습니다.
+        사용할 DB를 좌측 상단에서 선택하세요. 질문을 남기시면, DB에서 검색된 정보가 답변에 적용됩니다.
     </p>
 </header>
